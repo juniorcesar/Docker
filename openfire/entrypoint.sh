2bd623478dbc26e34f3e50d5d44c8453cd3f9d2e
Junior Cesar de Oliveira <juniorcesar.utfpr@gmail.com>
2016-11-03 17:13:45 -0200

    Openfire Dockerfile
    Criação do dockerfile para gerar imagem do Openfire em modo inicial de
    setup

---
+++ openfire/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ exec start-stop-daemon --start --chuid ${OPENFIRE_USER}:${OPENFIRE_USER} --exec /usr/bin/java -- \
+    -server \
+    -DopenfireHome=/usr/share/openfire \
+    -Dopenfire.lib.dir=/usr/share/openfire/lib \
+    -classpath /usr/share/openfire/lib/startup.jar \
+    -jar /usr/share/openfire/lib/startup.jar
